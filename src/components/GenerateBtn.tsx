3a4f900521939b38bddd404acd99df12174b77fe
Bavara Kumar Swami <kumarbevara7708@gmail.com>
2023-12-05 20:32:59 +0530

    upto uploadDnd

---
+++ src/components/GenerateBtn.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GenerateBtn() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default GenerateBtn
---
+++ src/store/useStore.ts
@@ -0,0 +1,6 @@
+import {create} from "zustand";
+
+export const useImage = create((set) => ({
+    imageUrl: undefined,
+    setImageUrl: (url:string) => set({imageUrl:url})
+}))
